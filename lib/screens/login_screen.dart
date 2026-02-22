5474ff8dc1d7aeb03b972e05486108810948a7e1
Chanul Wijesinghe <chanulwijesinghe12345@gmail.com>
2026-02-22 20:00:23 +0530

    Create a Login Screen dart file

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget{
+  const LoginScreen({super.key});
+
+  @Override
+  State<LoginScreen> createState()=> _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @Override
+  Widget build(BuildContext context){
+    return const Scaffold();
+  }
+}
