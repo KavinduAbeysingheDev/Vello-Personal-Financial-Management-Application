9e53e742a808d9822ea811a19cb39d8b2eb32d80
Chanul Wijesinghe <chanulwijesinghe12345@gmail.com>
2026-03-13 22:10:35 +0530

    scaffold AddTransactionScreen widget

---
+++ lib/screens/add_transaction_page.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import '../services/finance_service.dart';
+
+class AddTransactionScreen extends StatefulWidget {
+  const AddTransactionScreen({super.key});
+
+  @override
+  State<AddTransactionScreen> createState() => _AddTransactionScreenState();
+}
+
+class _AddTransactionScreenState extends State<AddTransactionScreen> {
+  final _formKey = GlobalKey<FormState>();
+  final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
+  final FinanceService _financeService = FinanceService();
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _amountController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
