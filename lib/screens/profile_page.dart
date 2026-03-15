569387d5d30d16426e55ae411c0f918a6a75adab
Chanul Wijesinghe <chanulwijesinghe12345@gmail.com>
2026-03-15 18:56:12 +0530

    import the packages and files to run the code

---
+++ lib/screens/profile_page.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import '../services/auth_service.dart';
+import '../models/user_model.dart';
+import 'login_screen.dart';
