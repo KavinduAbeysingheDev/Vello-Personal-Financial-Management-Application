0febcc54d8fe02921a6208695042810945d4f135
Chanul Wijesinghe <chanulwijesinghe12345@gmail.com>
2026-03-16 08:46:59 +0530

    get the user data from the database

--- lib/screens/profile_page.dart
+++ lib/screens/profile_page.dart
@@ -3,3 +3,37 @@ import 'package:firebase_auth/firebase_auth.dart';
 import '../services/auth_service.dart';
 import '../models/user_model.dart';
 import 'login_screen.dart';
+
+class ProfileScreen extends StatefulWidget {
+  const ProfileScreen({super.key});
+
+  @override
+  State<ProfileScreen> createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  final AuthService _authService = AuthService();
+  UserModel? _userModel;
+  bool _isLoading = true;
+  bool _isSendingReset = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadUserData();
+  }
+
+  Future<void> _loadUserData() async {
+    try {
+      final uid = FirebaseAuth.instance.currentUser?.uid;
+      if (uid != null) {
+        final user = await _authService.getUserData(uid);
+        setState(() {
+          _userModel = user;
+          _isLoading = false;
+        });
+      }
+    } catch (e) {
+      setState(() => _isLoading = false);
+    }
+  }
