4c55719d0bee8e3ab715dbda1b7ee0daccb1a2f8
Chanul Wijesinghe <chanulwijesinghe12345@gmail.com>
2026-03-18 16:53:14 +0530

    backend for sign with email

---
+++ lib/services/auth_service.dart
@@ -0,0 +1,26 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import '../models/user_model.dart';
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  // Get current user
+  User? get currentUser => _auth.currentUser;
+
+  // Stream of auth changes
+  Stream<User?> get authStateChanges => _auth.authStateChanges();
+
+  // Sign in with email and password
+  Future<UserCredential> signInWithEmail(String email, String password) async {
+    try {
+      UserCredential result = await _auth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return result;
+    } catch (e) {
+      rethrow;
+    }
+  }
